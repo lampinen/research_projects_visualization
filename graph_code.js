d4bc6f6af2e2d583d8f8fda5909319971c4f56ba
Andrew Lampinen <lampinen@stanford.edu>
2019-11-11 22:39:12 -0800

    A little progress

--- graph_code.js
+++ graph_code.js
@@ -21,18 +21,108 @@ function build_graph(projects) {
                 attribute_indices[this_attribute] = attribute_id;
                 attribute_id++;
             }
-            links.push([i, attribute_indices[this_attribute]])
+            links.push({source: i, target: attribute_indices[this_attribute]})
         }
     }
     return {nodes: projects.concat(attributes),
-            links: links};
+            edges: links};
 }
 
 
-function display_graph(graph) {
+function display_graph(raw_data) {
+    //Some code cannabalized from https://www.d3-graph-gallery.com/graph/network_basic.html
+    const width = 1024;
+    const height = 1024;
+    const radius = height/32;
+    const fontsize = "32pt";
+    const text_dx = -radius/3;
+    const text_dy = radius/2;
+
+    const colors = {project: "#6600CC", 
+                    attribute: "#00CC00"};
+
+    const font_colors = {project: "FFFFFF", 
+                         attribute: "#000000"};
+
+    const positions = {project: 3 * width / 4, 
+                       attribute: width / 4};
+
+    var data = build_graph(raw_data);
+    var num_nodes = data.length;
+
     var svg = d3
         .select("#svg-container")
         .append("svg")
-        .attr("width", 1024)
-        .attr("height", 1024);
+        .attr("width", width)
+        .attr("height", height)
+        .append("g");
+
+    // gradient for edges
+    var svgDefs = svg.append("defs");
+    var edgeGradient = svgDefs.append("linearGradient")
+        .attr("id", "edgeGradient");
+
+    edgeGradient.append("stop")
+        .attr("class", "start")
+        .attr("offset", "0%")
+        .attr("stop-color", colors.attribute);
+
+    edgeGradient.append("stop")
+        .attr("class", "end")
+        .attr("offset", "100%")
+        .attr("stop-color", colors.project);
+
+    // edge and node styles
+    var link = svg.append("g")
+        .selectAll("line")
+        .data(data.edges)
+        .enter()
+        .append("line")
+            .attr("stroke-width", radius/8)
+            .style("stroke", "url(#edgeGradient)")
+
+    var node = svg.append("g")
+        .selectAll(".node")
+        .data(data.nodes)
+        .enter()
+        .append("g")
+
+
+    var node_circles = node
+      .append("circle")
+          .attr("r", radius)
+          .style("fill", d => colors[d.type])
+
+    var node_text = node
+        .append("text")
+            .attr("font-size", fontsize)
+            .text(d => d.id)
+              .style("fill", d => font_colors[d.type])
+
+    var simulation = d3.forceSimulation(data.nodes)                 
+        .force("x", d3.forceX().x(d => positions[d.type]))
+        .force("link", d3.forceLink()                               // This force provides links between nodes
+              .id(function(d) { return d.id; })                     // This provide  the id of a node
+              .links(data.edges)                                    // and this the list of links
+              .strength(0.01)
+        )
+        .force("charge", d3.forceManyBody().strength(-300))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+        .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
+        .on("end", ticked);
+        
+        
+    // This function is run at each iteration of the force algorithm, updating the nodes position.
+    function ticked() {
+      link
+          .attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
+
+      node.attr("transform", function(d) {
+          return "translate(" + d.x + "," + d.y + ")";
+      });
+
+    }
+
 }
